8a77b2ae40e0f9e6d02df7481ea624390a08d3b4
Devin McGloin <devin@mcgloin.email>
2021-07-28 12:34:10 -0700

    manual

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,25 @@
 import "../styles/globals.css";
-import TagManager from "react-gtm-module";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-const tagManagerArgs = {
-  gtmId: "G-4RWBNY6D03",
-};
+const pageview = (url) =>
+  window.gtag("config", "G-4RWBNY6D03", {
+    page_path: url,
+  });
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-  }, []);
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,24 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-4RWBNY6D03`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4RWBNY6D03', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body className="dark:bg-off-black">
           <Main />
           <NextScript />
