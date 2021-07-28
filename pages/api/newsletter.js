235d314425f4eb5440341231e349d73b2594052d
Devin McGloin <devin@mcgloin.email>
2021-07-28 11:27:56 -0700

    zapier

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,8 +1,16 @@
 export default function handler(req, res) {
   const { name, email } = req.body;
 
+  fetch("https://hooks.zapier.com/hooks/catch/6111089/b2xo0aq/", {
+    method: "POST",
+    body: JSON.stringify({ name, email }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   res.status(200).json({
     status: 200,
-    message: `Thanks for subscribing ${name} (${email}), We'll be in touch! `,
+    message: `Thanks for subscribing ${name} (${email}), We'll be in touch!`,
   });
 }
