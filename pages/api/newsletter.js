80280b3029c461b15c8e95fcaac2d353b05d4d26
Devin McGloin <devin@mcgloin.email>
2021-07-28 11:32:35 -0700

    post function

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,13 +1,27 @@
+const postToZapier = async (name, email) => {
+  const res = await fetch(
+    "https://hooks.zapier.com/hooks/catch/6111089/b2xo0aq/",
+    {
+      method: "POST",
+      body: JSON.stringify({ name, email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    throw new Error(data.message);
+  }
+  return data;
+};
+
 export default function handler(req, res) {
   const { name, email } = req.body;
 
-  fetch("https://hooks.zapier.com/hooks/catch/6111089/b2xo0aq/", {
-    method: "POST",
-    body: JSON.stringify({ name, email }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const zapierResponse = postToZapier(name, email);
+  console.log(zapierResponse);
 
   res.status(200).json({
     status: 200,
