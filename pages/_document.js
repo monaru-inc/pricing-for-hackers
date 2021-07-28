47456665aca776f6b679c8f2486c41be4760fb34
Devin McGloin <devin@mcgloin.email>
2021-07-28 12:06:32 -0700

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4RWBNY6D03"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-4RWBNY6D03');
+        </script>
+        </Head>
         <body className="dark:bg-off-black">
           <Main />
           <NextScript />
